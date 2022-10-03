42adc488a6a6633ad79b9c44d9afda06c3799e15
unknown <tyeik@gktechinc.com>
2022-10-03 13:05:16 -0600

    Initial

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AppShell;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace AppShell;
+
+public partial class AppShell : Shell
+{
+    private bool _subItemVisible;
+    public AppShell()
+    {
+        InitializeComponent();
+
+        _subItemVisible = true;
+    }
+
+    private void TapGestureRecognizer_Tapped(object sender, System.EventArgs e)
+    {
+        _subItemVisible = !_subItemVisible;
+        
+        Resources["myVisible"] = _subItemVisible;
+        ForceLayout();
+        InvalidateMeasure();
+    }
+
+    private void menuItem2_Clicked(object sender, EventArgs e)
+    {
+        var item = Items[4];
+        item.FlyoutItemIsVisible = _subItemVisible;
+        item.IsVisible = _subItemVisible;
+    }
+}
